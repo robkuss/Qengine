bba9f90cc1b1c74a28bd2548862dd3260204c571
Robson <robert.kuss@gmx.net>
2024-11-15 08:16:35 +0100

    went back to defining status codes with #define because it's funny

--- src/Qengine.cpp
+++ src/Qengine.cpp
@@ -9,9 +9,7 @@ constexpr int DEFAULT_WIDTH  = 1920;
 constexpr int DEFAULT_HEIGHT = 1080;
 
 int main() {
-	std::cout << SPECIFIC_ERROR << std::endl;
-
-    try {
+	try {
         // Initialize and start Viewport
         Viewport(TITLE, DEFAULT_WIDTH, DEFAULT_HEIGHT, SceneManager()).start();
     } catch (const std::exception &e) {
